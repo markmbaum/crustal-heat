57d41f907be81156ad5c602c715485ff69e684b5
Mark Baum <markbaum@g.harvard.edu>
2020-02-07 10:05:37 -0500

    fix readme mistake

--- src/heat.h
+++ src/heat.h
@@ -5,7 +5,7 @@
 /*!
 \mainpage crustal_heat
 
-This repository provides a class for solving the one-dimensional heat equation with mixed boundary conditions, a temperature at the surface and a geothermal gradient at the bottom. It allows spatially variable thermal properties and time-varying boundary conditions. The class uses finite volumes and a time stepper from [libode](https://github.com/wordsworthgroup/libode). It also chooses its own time step to maximize efficiency. Latent heat can be enabled to simulate freezing and thawing of ground ice/water. Openmp and the class-based structure of this code make it good for running a large number of independent integrations. Simulating crustal heat transfer over wide ranges of initial conditions and/or physical parameters.
+This repository provides a class for solving the one-dimensional heat equation with mixed boundary conditions, a temperature at the surface and a geothermal gradient at the bottom. It allows spatially variable thermal properties and time-varying boundary conditions. The class uses finite volumes and a time stepper from [libode](https://github.com/wordsworthgroup/libode). It also chooses its own time step to maximize efficiency. Latent heat can be enabled to simulate freezing and thawing of ground ice/water. Openmp and the class-based structure of this code make it good for running a large number of independent integrations with wide ranges of initial conditions and/or physical parameters.
 
 The code runs through three classes/structs. To perform an integration:
     1. Create a Settings struct by reading values from a text file. An example settings file is included in the repository as settings.txt. The Settings struct contains physical parameters, integration settigns, and grid specifications.
